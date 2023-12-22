11d6ae5a7d3ff98ed1e5122f260c5d3d6f13f678
Sapan Gajjar <sapangajjar101105@gmail.com>
2023-12-22 14:28:40 +0530

    initial release

---
+++ README.md
@@ -0,0 +1,57 @@
+# OTA Builder for Port ROMs 🛠️
+```
+==========================================
+ota builder for port roms
+
+[Place Build.prop & super.img inside input]
+Note: Keep a Backup of super.img
+==========================================
+```
+For issues contact [isg32 Telegram](https://t.me/isg32)
+
+> __Overview__
+This script automates the process of building an OTA package for ported ROMs. It gathers necessary information from the provided build.prop file and compresses the super.img. The resulting ROM package is stored in the Build folder.
+
+>Prerequisites
+
+__Input Requirements:__
+* build.prop file with essential ROM information.
+* super.img file (Backup recommended).
+* Usage
+* Clone the repository.
+```bash
+ git clone https://github.com/yourusername/otabuilder.git
+ cd otabuilder
+
+```
+* Place the required files in the input directory.
+* Add Your Firmware files in the output/BuildFiles/Firmware folder & edit the essentials in output/BuildFiles/META-INF/com/google/android - update-binary file.
+* Run the script.
+
+```bash
+python main.py
+```
+Find the completed ROM package in the Build folder.
+Script Details
+The script performs the following tasks:
+
+Retrieves variables from build.prop.
+Compresses super.img.
+Moves super.img to the output/BuildFiles/image directory.
+Writes an installer script (update-binary) with ROM details.
+Creates a payload zip file.
+Stores the final ROM package in the build directory.
+Script Execution
+The script is executed using the L1() function. It is divided into several sections, each performing a specific task:
+
+Retrieving variables from build.prop.
+Compressing super.img.
+Writing the installer script.
+Creating the payload zip file.
+Completing the ROM build.
+Please ensure you have the necessary permissions and dependencies installed before running the script.
+
+> __Notes:__
+* Backup your super.img file before running the script.
+* Add Your Firmware files in the output/BuildFiles/Firmware folder & edit the essentials in output/BuildFiles/META-INF/com/google/android - update-binary file.
+* Contact isg32's Telegram for support.
---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+
+def L1():
+    print("""
+ ==========================================
+|       ota builder for port roms          |
+ ==========================================
+|   For issues contact https://t.me/isg32  |
+|                                          |
+|[Place Build.prop & super.img inside input]
+|    Note: Keep a Backup of super.img      | 
+ ==========================================\n""")
+    os.system("""
+sleep 1
+clear
+sleep 1
+cd input
+
+echo "retrieving vars from build.prop"
+sleep 0.5
+build=$(cat build.prop | grep ro.system.build.id | cut -d "=" -f 2)
+sysname=$(cat build.prop | grep ro.product.system.name | cut -d "=" -f 2)
+androidver=$(cat build.prop | grep ro.build.version.release | cut -d "=" -f 2)
+buildfp=$(cat build.prop | grep ro.system.build.fingerprint | cut -d "=" -f 2)
+securitypatch=$(cat build.prop | grep ro.build.version.security_patch | cut -d "=" -f 2)
+androidversion=$(cat build.prop | grep ro.build.version.release_or_codename | cut -d "=" -f 2)
+
+echo " "
+echo "===="
+echo "TARGET_NAME = $build"
+echo "BUILD_FINGP = $buildfp"
+echo "===="
+sleep 0.5
+              
+echo "Compressing Super..."
+#gzip -f super.img
+echo "Done! "
+sleep 0.5
+echo "Compressing Complete! "
+echo "Moving Super to package..." 
+#mv super.img.gz ../output/BuildFiles/image
+sleep 0.5
+echo "Done!"
+
+sleep 0.5
+cd ..
+outrom="$build $build"
+buildfp_text="Fingerprint: $buildfp"
+build_text="Build: $build"
+patch="Security Patch: $securitypatch"
+android="Android: $androidversion"
+cd metadata
+
+sleep 0.5
+echo " "
+echo "Writiing Installer Script..."
+# Create a temporary file with the updated content
+sed -e '21s|.*|ui_print "'"$outrom"'"|' -e '22s|.*|ui_print "'"$buildfp_text"'"|' -e '23s|.*|ui_print "'"$build_text"'"|' -e '25s|.*|ui_print "'"$patch"'"|'  -e '24s|.*|ui_print "'"$android"'"|' update-binary > update-binary.tmp
+
+# Overwrite the original file with the temporary file
+mv update-binary.tmp update-binary
+cp update-binary ../output/BuildFiles/META-INF/com/google/android
+cd ..
+cd output/BuildFiles
+echo "Done!"
+sleep 0.5
+              
+clear
+echo "Creating Payload..."
+echo " "
+zip -r $sysname-$build-$androidversion.0-Hanoip.zip *
+mv $sysname-$build-$androidversion.0-Hanoip.zip ../../build
+
+clear
+echo "  Complete you can find the rom in Build folder"
+""")
+
+L1()
